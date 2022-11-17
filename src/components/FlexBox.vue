a791c8ea5b3111a641dc317c8676eadfb356a40f
andmohiko <andmohiko@gmail.com>
2022-11-17 09:21:41 +0900

    Nuxt3 initial commit

---
+++ src/components/FlexBox.vue
@@ -0,0 +1,58 @@
+<template>
+  <div
+    class="flexBox"
+    :style="{
+      gap: `${gap}px`,
+      flexDirection: direction,
+      padding: `${pt || py}px ${pr || px}px ${pb || py}px ${pl || px}px`
+    }"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FlexBox',
+  props: {
+    gap: {
+      type: Number,
+      default: 0
+    },
+    direction: {
+      type: String,
+      default: 'column'
+    },
+    py: {
+      type: Number,
+      default: 0
+    },
+    px: {
+      type: Number,
+      default: 0
+    },
+    pt: {
+      type: Number,
+      default: 0
+    },
+    pr: {
+      type: Number,
+      default: 0
+    },
+    pb: {
+      type: Number,
+      default: 0
+    },
+    pl: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+.flexBox {
+  display: flex;
+}
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <FlexBox>
+    <h1>たいとる</h1>
+    <p>これは段落です</p>
+  </FlexBox>
+</template>
